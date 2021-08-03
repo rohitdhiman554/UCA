16b07854b86d9fa2071da76cf5467520b0d2534e
vagrant <vagrant@myvm18>
2021-08-03 07:31:16 +0000

    pyramid

---
+++ C_Revision/ques2.c
@@ -0,0 +1,26 @@
+/* 
+ * Print Pyramid
+ * @Rohit Dhiman (1910991113),28/07/2021
+ * assignment: C Foundations(question 2)
+ */
+#include <stdio.h>
+
+int main()
+{
+
+    int n=5;
+
+  for(int i=1;i<=n;i++)
+  {
+      for(int space=n-i-1;space>=0;space--)
+      {
+          printf(" ");
+      }
+      for(int j=0;j<i;j++)
+      {
+          printf("# ");
+      }
+      printf("\n");
+  }
+}
+
