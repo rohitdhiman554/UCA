b9e113db2ea2f0356f50709c2d2b4577c813113a
Rohit Dhiman <72524731+rohitdhiman554@users.noreply.github.com>
2021-08-07 00:05:57 +0530

    fib.c

--- fib.c
+++ fib.c
@@ -14,10 +14,10 @@
 #include <stdio.h>
 int fib(int n)
 {
-    if(n==0)
+    if(n==0)//base condition
     return 0;
     
-    if(n==1||n==2)
+    if(n==1||n==2)//base condition
     return 1;
     
     return fib(n-1)+fib(n-2);
@@ -27,8 +27,8 @@ int fib(int n)
 
 int main()
 {
-   int n=3;
-   int arr[20];
+   int n=3;//n defines how many times we have to print third element of fibonacci series.
+   int arr[20];//Example (array having size 20 to show fibonacci series upto 20 elements.)
    int len=sizeof(arr)/sizeof(arr[0]);
    for(int i=0;i<len;i++)
    {
@@ -36,7 +36,7 @@ int main()
    }
    
    printf("\n");
-   for(int i=0;i<n;i++)
+   for(int i=0;i<n;i++)//loop to print n times every third element of series.
    {
        printf("%d ",arr[i*n+3]);
    }
